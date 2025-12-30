315836a96f98f08bc84308524526c5a6e36fc106
Ankit-2869 <ates.ankit@gmail.com>
2025-12-30 20:24:42 +0530

    Update Resturant_billing.c
    i have added customer mobile number input and improved billing logic.
    Fixed input handling issues and updated output formatting.

--- Project/Resturant_billing.c
+++ Project/Resturant_billing.c
@@ -1,4 +1,10 @@
+//Ankit kumar (ERP: RU-25-10211)
+//Project number 13 : Restaurant billing system
+// Problem statement: Restaurant billing system. Accept orders, generate detailed bill with GST and discounts
+
 #include <stdio.h>
+#include <string.h>
+
 int main() {
     char name[50];
     char mobile[15];
@@ -6,10 +12,16 @@ int main() {
     float total = 0, gst, discount = 0, finalTotal;
 
     printf("Welcome to Our Restaurant!\n");
-    printf("Please enter Customer Name: ");
-    scanf("%s", name);
+
     printf("Please enter Mobile Number: ");
     scanf("%s", mobile);
+
+    getchar(); // clear newline
+
+    printf("Please enter Customer Name: ");
+    fgets(name, sizeof(name), stdin);
+
+    name[strcspn(name, "\n")] = '\0';
 
     printf("\n--- MENU ---\n");
     printf("1. Burger       - Rs 100\n");
@@ -19,7 +31,6 @@ int main() {
     printf("5. Coke         - Rs 50\n");
     printf("0. Finish & Print Bill\n");
 
-    // --- 
     while (1) {
         printf("\nEnter Item Number: ");
         scanf("%d", &choice);
@@ -38,10 +49,12 @@ int main() {
             default: printf("Invalid Item!\n");
         }
     }
-    gst = total * 0.18; // 
+
+    gst = total * 0.18;
     if (total > 500) {
-        discount = total * 0.10; // if coustomer adds item more then worth of 500 he will get discount of 10%
+        discount = total * 0.10;
     }
+
     finalTotal = total + gst - discount;
 
     printf("\n==============================");
@@ -56,6 +69,7 @@ int main() {
     printf("\n------------------------------");
     printf("\nGRAND TOTAL: Rs %.2f", finalTotal);
     printf("\n==============================");
-    printf("\n  Thank you! Visit Again.  \n");
+    printf("\n  Thank you! Visit Again.\n");
+
     return 0;
 }
